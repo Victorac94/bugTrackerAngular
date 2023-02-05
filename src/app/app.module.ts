143b9fb04914a61f3fbe18dc129a457923446262
Victor Antunez <victorac94@hotmail.com>
2023-02-05 15:17:50 +0100

    Improve UX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,7 +61,7 @@ import { IssueComposerComponent } from './issue-composer/issue-composer';
         syntax: true,
         toolbar: [
           [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline', 'strike'],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
           [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
           ['link', 'code-block']
         ]
